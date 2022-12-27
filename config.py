3f500b7a46e3b427869cdabfbbb6d5dad0e4f5e8
LegendBoy XD <97836367+LEGEND-AI@users.noreply.github.com>
2022-12-28 01:09:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,16 +3,16 @@ from sample_config import Config
 
 class Development(Config):
     # get this values from the my.telegram.org
-    APP_ID = 8460373
-    API_HASH = "83d8e423197251216303abfcbed9e820"
+    APP_ID = 17881150
+    API_HASH = "83e83a14784a5355838dd0c24529d52a"
     # the name to display in your alive message
     ALIVE_NAME = "LegendBoy"
     # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
     DB_URI = "postgres://oxkxqagp:846JSW5Jb4sbPZNlowgqkS_HYfWOFyoa@snuffleupagus.db.elephantsql.com/oxkxqagp"
     # After cloning the repo and installing requirements do python3 telesetup.py an fill th this
-    LEGEND_STRING = "1BVtsOGcBu3ws8fazY8lm0G69p74TEjWIpLTbio2Qb0RpZfD5oqgxl1vM2ReCBKKD53u2VkBthC9NeFJAN2r7tzjrJyb0_vXCF6X-yb0vLi11CAWT8-BAQVih_0Uw4EI_W9CvyNwZYHN-rRYROqFDsj766wnuHfaepxl3lAFRm-DZ4e9wKNs9JP3JV_zo4xWfrFu8_GbLYD8tUtAOKjWU4bREXMBvwS96TX7sItXoLomoStJOgwGrBEdufXe_zIwGYBdzzXUFyI6WGNXvMdqza1-JMRpawihXDV9QQgL5IUAuZBbT0M5jbdK-khTo3JfmU6ykDk09_q1dTWost3JhdgGdC7DiQ5Q="
+    LEGEND_STRING = "1AZWarzUBuxvy7WfsPwYssPg2r4fZI2qOFTAGfCO4p1yVN-qK77_xnrlBJg9vse3vLGhOBtPYlvJDSn5sW-3Uml2C2fEeM1XJXsL5k2jHc50aPHpgvDr5J2gdlPCH32HNoQnWOrYaX_pymBc0oTLmVcagwUmkWEdt1gyGyvCt75k_vJw3un_TFZBjWlJgKUKsRvTXkImcZY0rDgFXijl7wjO3Ict9EH5H6zHl69U3m3pPwmzk6yldfbUTcPQk_5Iv6lv6rOWy3xussxP0KimTmYjH5ELUDTvjJ_Pjw9pq8NsFwoG8hvXPsyOGqqjZQuR5UcOkIUead2NoX2hqpwWxoIThVs9uVPE="
     # create a new bot in @botfather and fill the following vales with bottoken
-    BOT_TOKEN = "5132044795:AAHXkYsT1HLvPOEsNN3B7l9tekJvrVZknwg"
+    BOT_TOKEN = "5540365226:AAGaOTQYFSEhGqhRx0cFev6heCDI1JZqsCo"
     # command handler
     HANDLER = "."
     # command hanler for sudo
