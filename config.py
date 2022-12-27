deea0b31bd4dd6e1d7af5aa43abfd1ab0b20e98e
LegendBoy XD <97836367+LEGEND-AI@users.noreply.github.com>
2022-12-28 01:21:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Development(Config):
     # the name to display in your alive message
     ALIVE_NAME = "LegendBoy"
     # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
-    DB_URI = "postgres://oxkxqagp:846JSW5Jb4sbPZNlowgqkS_HYfWOFyoa@snuffleupagus.db.elephantsql.com/oxkxqagp"
+    DB_URI = "postgres://hwffqxra:LZ0wJmLXRv9rCsgTJf5Cnp-i1lqU2YCl@mahmud.db.elephantsql.com/hwffqxra"
     # After cloning the repo and installing requirements do python3 telesetup.py an fill th this
     LEGEND_STRING = "1AZWarzUBuxvy7WfsPwYssPg2r4fZI2qOFTAGfCO4p1yVN-qK77_xnrlBJg9vse3vLGhOBtPYlvJDSn5sW-3Uml2C2fEeM1XJXsL5k2jHc50aPHpgvDr5J2gdlPCH32HNoQnWOrYaX_pymBc0oTLmVcagwUmkWEdt1gyGyvCt75k_vJw3un_TFZBjWlJgKUKsRvTXkImcZY0rDgFXijl7wjO3Ict9EH5H6zHl69U3m3pPwmzk6yldfbUTcPQk_5Iv6lv6rOWy3xussxP0KimTmYjH5ELUDTvjJ_Pjw9pq8NsFwoG8hvXPsyOGqqjZQuR5UcOkIUead2NoX2hqpwWxoIThVs9uVPE="
     # create a new bot in @botfather and fill the following vales with bottoken
